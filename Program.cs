c832d4765d6255f49aa03e4196047edf71ec2aba
Сергей Усков <q3mydoom@hotmail.com>
2021-10-25 16:56:32 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Homework_2._7_Task_1
+{
+    class Program
+    {
+        static void Main()
+        {
+            //переменные
+            string Name = "Сергей";
+            string Surname = "Усков";
+            string Secondname = "Евгеньевич";
+            int ProgrammingGrade = 1;
+            int MathsGrade = 2;
+            int PhysicsGrade = 3;
+                    
+            Console.WriteLine(" Name: {0}\n Secondname: {1}\n Surname: {2}\n", Name, Secondname, Surname);
+            Console.WriteLine(" Programming Grade: {0}\t Maths Grade: {1}\t Physics Grade: {2}\t", ProgrammingGrade, MathsGrade, PhysicsGrade);
+
+            Console.ReadKey();
+        }
+    }
+}
